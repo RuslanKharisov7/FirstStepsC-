99577661d1df51b4ec1d530506890ad6dedd8f79
KharisovR <alkatrazrudi@gmail.com>
2023-01-26 00:01:38 +0200

    Решили первое задание

---
+++ Ex000/Program.cs
@@ -0,0 +1,7 @@
+﻿//  0.Вывести квадрат числа**
+double numberA = new Random().Next();
+Console.WriteLine("Случайное число : " + numberA);
+Console.WriteLine("Квадрат заданного числа :" + numberA*numberA);
+
+
+
