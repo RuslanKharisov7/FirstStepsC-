d7fcf4ce226183fb420ed0e76636af68718242d4
KharisovR <alkatrazrudi@gmail.com>
2023-01-27 23:27:47 +0200

    Second commit ex2

---
+++ Ex000Var2/Program.cs
@@ -0,0 +1,6 @@
+﻿// 0.Вывести квадрат числа**
+using System.Globalization;
+Console.WriteLine("Введите число которое желаете возвести в квадрат, дробные числа вводятся через запятую");
+double? numberA=double.Parse(Console.ReadLine()!);
+Console.WriteLine("Квадрат введенного числа :" + numberA * numberA);
+
