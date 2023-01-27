614a23647f947ad747d6187f7796f4e4f542dc27
KharisovR <alkatrazrudi@gmail.com>
2023-01-27 23:50:34 +0200

    Решили вторую задачу

---
+++ Ex001/Program.cs
@@ -0,0 +1,22 @@
+﻿// 1.По двум заданным числам проверять является ли первое квадратом второго**
+
+double numberA = new Random().Next(0, 10);
+Console.WriteLine("Первое число: " + numberA);
+double numberB = new Random().Next(0, 10);
+Console.WriteLine("Второе число:" + numberB);
+if (numberA/numberB == numberA)
+{
+    Console.WriteLine("Первое число является квадратом второго");
+}
+if (numberB == 0)
+{
+    Console.WriteLine("Деление на ноль не возможно");
+}
+if (numberA == 0)
+{
+    Console.WriteLine("При деление 0 на любое число, результат будет 0");
+}
+ else
+ {
+    Console.WriteLine("Первое число не является квадратом второго");
+ }
