b802f1edc7425a740886faf92be9e3e163da1e01
KharisovR <alkatrazrudi@gmail.com>
2023-01-29 15:59:42 +0200

    Решили задачу номер 9

---
+++ Ex009/Program.cs
@@ -0,0 +1,4 @@
+﻿//  9.Показать последнюю цифру трёхзначного числа**
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Последняя цифра числа {number} это цифра {number%10}");
