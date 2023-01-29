66e55b942e388194621e2c68c554998ea2056c19
KharisovR <alkatrazrudi@gmail.com>
2023-01-29 14:24:32 +0200

    Решили первую задачу третим методом

---
+++ Ex001Var3/Program.cs
@@ -0,0 +1,23 @@
+﻿// 1.По двум заданным числам проверять является ли первое квадратом второго**
+
+Console.WriteLine("Введите первое число");
+double numberA = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+double numberB = double.Parse(Console.ReadLine()!);
+
+if (numberA/numberB == numberB)
+{
+    Console.WriteLine($"Число {numberA} является квадратом числа {numberB}");
+}
+ else
+ {
+    Console.WriteLine($"Число {numberA} не является квадратом числа {numberB}");
+ }
+if (numberA == 0)
+{
+    Console.WriteLine("При деление 0 на любое число ответ будет 0");
+}
+ if (numberB == 0)
+ {
+    Console.WriteLine("Деление на 0 не возможно");
+ }
