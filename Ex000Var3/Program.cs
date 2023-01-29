4eb229ede9dbadff40db493a84598168fc492699
KharisovR <alkatrazrudi@gmail.com>
2023-01-29 13:52:04 +0200

    Решили задачу 0 немного иначе

---
+++ Ex000Var3/Program.cs
@@ -0,0 +1,6 @@
+﻿//  0.Вывести квадрат числа**
+Console.WriteLine("Введите число которое желаете возвести в квадрат");
+double number = double.Parse(Console.ReadLine()!);
+double sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
+
