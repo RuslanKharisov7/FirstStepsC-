fbb9b76023d45b34395869af235dd4ffb071e42a
KharisovR <alkatrazrudi@gmail.com>
2023-01-30 00:22:42 +0200

    Решили 8 задачу

---
+++ Ex008/Program.cs
@@ -0,0 +1,11 @@
+﻿// 8.Показать четные числа от 1 до N
+
+Console.WriteLine("Введите число");
+double number = double.Parse(Console.ReadLine()!);
+double M = 1;
+while (M<=number)
+{
+    if (M % 2 ==0)
+    Console.Write($"{M} ");
+    M++;
+}
