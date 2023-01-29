84baa6a24b9a834e84c81ac4bc71d1ee82149c14
KharisovR <alkatrazrudi@gmail.com>
2023-01-29 16:29:54 +0200

    Внесли условие в задачу 9

--- Ex009Var2/Program.cs
+++ Ex009Var2/Program.cs
@@ -3,6 +3,11 @@ Console.WriteLine("Введите трехзначное число");
 string s = Console.ReadLine();
 int number =  int.Parse(s);
 int y = number%10;
+if (s.Length !=3)
+{
+    Console.Write("Число не является трехзначным");
+    return;
+}
 string lastnumber = Convert.ToString(s[2]);
 Console.WriteLine($"Последняя цифра заданого чисала {lastnumber}");
 
