36439daa9f48c90efc10b5566b35f9453f4e0bd6
KharisovR <alkatrazrudi@gmail.com>
2023-01-29 16:45:28 +0200

    Решили 9 задачу с использованием цикла while

---
+++ Ex009Var3/Program.cs
@@ -0,0 +1,11 @@
+﻿// //  9.Показать последнюю цифру трёхзначного числа**
+
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+while (number<100 || number>999)
+{
+    Console.WriteLine("Введенное число не является треззначным, введите число снова");
+    Console.WriteLine("Введите трехзначное число");
+    number = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine($"Последняя цифра введенного числа {number%10} ");
