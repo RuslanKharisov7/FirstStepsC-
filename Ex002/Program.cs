756b0f6492a4654d5dee4daff152df66c025ee61
KharisovR <alkatrazrudi@gmail.com>
2023-01-29 23:11:47 +0200

    Решили 2 задание

---
+++ Ex002/Program.cs
@@ -0,0 +1,14 @@
+﻿// 2.Даны два числа. Показать большее и меньшее число**
+
+Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите ыторое число");
+int numberB = int.Parse(Console.ReadLine()!);
+if (numberA>numberB)
+{
+    Console.WriteLine($"Большее число {numberA}, меньшее число {numberB}");
+}
+else
+{
+    Console.WriteLine($"Большее число {numberB}, меньшее число {numberA}");
+}
