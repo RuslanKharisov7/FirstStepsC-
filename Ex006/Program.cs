53a9be6ec99ebaa24edede578aa693fe271ba7c4
KharisovR <alkatrazrudi@gmail.com>
2023-01-29 23:51:14 +0200

    Решили 6 задачу

---
+++ Ex006/Program.cs
@@ -0,0 +1,11 @@
+﻿// 6.Выяснить является ли число чётным
+Console.WriteLine("Введите число для проверки на четность");
+int number = int.Parse(Console.ReadLine()!);
+if (number%2 == 0)
+{
+    Console.WriteLine("Введенное число является четным");
+}
+else 
+{
+    Console.WriteLine("Число не является четным ");
+}
