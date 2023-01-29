6101a7a908790e7dae1896c85adc9f5e5bf1c22d
KharisovR <alkatrazrudi@gmail.com>
2023-01-29 16:22:58 +0200

    Решили задачу 9, вторым способом

--- Ex009/Program.cs
+++ Ex009/Program.cs
@@ -1,4 +1,9 @@
 ﻿//  9.Показать последнюю цифру трёхзначного числа**
 Console.WriteLine("Введите трехзначное число");
 int number = int.Parse(Console.ReadLine()!);
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Число не является трехзначным");
+    return;
+}
 Console.WriteLine($"Последняя цифра числа {number} это цифра {number%10}");
---
+++ Ex009Var2/Program.cs
@@ -0,0 +1,8 @@
+﻿// //  9.Показать последнюю цифру трёхзначного числа**
+Console.WriteLine("Введите трехзначное число");
+string s = Console.ReadLine();
+int number =  int.Parse(s);
+int y = number%10;
+string lastnumber = Convert.ToString(s[2]);
+Console.WriteLine($"Последняя цифра заданого чисала {lastnumber}");
+
