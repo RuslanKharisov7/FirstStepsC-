1ecab7d4c3aec165939873e9b6dd35f87ca2a1e9
KharisovR <alkatrazrudi@gmail.com>
2023-01-30 22:27:15 +0200

    Решили задачу на семинаре

---
+++ Ex0001/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу которая выводит случайное число из отрезка [10, 99] и показывает наибольшую цифру числа
+
+Console.Clear();
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Число {number}");
+int num1 = number/10;
+int num2 = number%10;
+if (num1>num2)
+{
+    Console.WriteLine($"Наибольшая цифра числа {number}, цифра {num1}");
+}
+else 
+{
+    Console.WriteLine($"Наибольшая цифра числа {number}, цифра {num2}");
+}
