aacf268b1339d9f8d2bdef30ab626a4b521beb1b
KharisovR <alkatrazrudi@gmail.com>
2023-01-30 23:12:57 +0200

    Решили второе задание семинара, еще одним способом

---
+++ Ex012Var2/Program.cs
@@ -0,0 +1,6 @@
+﻿// // 12.Удалить вторую цифру трёхзначного числа
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+int result = (number/100)*10 + number%10;
+Console.WriteLine(result);
