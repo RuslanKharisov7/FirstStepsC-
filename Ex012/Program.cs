92e2191f37a0cdedf72abd91785c4871bc1c6c8f
KharisovR <alkatrazrudi@gmail.com>
2023-01-30 22:59:24 +0200

    решили 2 задание на семинаре

---
+++ Ex012/Program.cs
@@ -0,0 +1,8 @@
+﻿// 12.Удалить вторую цифру трёхзначного числа
+Console.Clear();
+int number = new Random().Next(100, 1000);
+Console.WriteLine($"Число {number}");
+int num1 = number/100;
+int num2 = (number/10)%10;
+int num3 = number%10;
+Console.WriteLine($"{num1}{num3}");
