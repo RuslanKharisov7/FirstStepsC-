4d2b7b020b0962bab234f88b9211bdf49f020b27
KharisovR <alkatrazrudi@gmail.com>
2023-01-31 22:52:03 +0200

    Решили задачу 13 на семинаре

---
+++ Ex013/Program.cs
@@ -0,0 +1,15 @@
+﻿// 13.Выяснить, кратно ли число заданному, если нет, вывести остаток.
+Console.Clear();
+Console.WriteLine("Введите первое число");
+int numberA = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+int numberB = int.Parse(Console.ReadLine()!);
+int rez = numberA%numberB;
+if (numberA%numberB == 0)
+{
+    Console.WriteLine($"Первое число {numberA}, кратно второму числу {numberB}");
+}
+else
+{
+    Console.WriteLine($"Первое число {numberA}, не кратно второму числу {numberB} остаток от деления равен {rez}");
+}
