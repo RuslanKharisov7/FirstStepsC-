f14a6a97dd44d45ade053799bcafc360fdbef1c4
KharisovR <alkatrazrudi@gmail.com>
2023-01-31 23:09:19 +0200

    Решили 15 задачу на семинаре

---
+++ Ex015/Program.cs
@@ -0,0 +1,12 @@
+﻿// 15. Дано число. Проверить кратно ли оно 7 и 23
+Console.Clear();
+Console.WriteLine("Введите число для проверки на кратность 7 и 23");
+int number = int.Parse(Console.ReadLine()!);
+if (number%7 == 0 && number%23 == 0)
+{
+    Console.WriteLine($"Да кратно");
+}
+else
+{
+    Console.WriteLine("Нет не кратно");
+}
