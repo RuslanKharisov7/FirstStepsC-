fe4660b6c9289b787fba6f5044e139308760634a
KharisovR <alkatrazrudi@gmail.com>
2023-01-31 23:21:17 +0200

    Решили 17 задачу на семинаре

---
+++ Ex017/Program.cs
@@ -0,0 +1,14 @@
+﻿// 17. По двум заданным числам проверять является ли одно квадратом другого**
+Console.Clear();
+Console.WriteLine("Введите первое число");
+double numberA = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число");
+double numberB = double.Parse(Console.ReadLine()!);
+if (numberA % numberB == 0 || numberB % numberA == 0)
+{
+    Console.WriteLine("Одно число является квадратом другого");
+}
+else
+{
+    Console.WriteLine("Не одно из чисел не является квадрвтом другого");
+}
