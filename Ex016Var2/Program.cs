d70cb9946cd990e83c197cc5d0ba18463ef6c819
KharisovR <alkatrazrudi@gmail.com>
2023-02-02 00:06:24 +0200

    Решили 15 задачу,из домашнего задания  еще одним способом

---
+++ Ex016Var2/Program.cs
@@ -0,0 +1,16 @@
+﻿// 16. Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+Console.Clear();
+Console.WriteLine("Введите номер дня недели");
+int day = int.Parse(Console.ReadLine()!);
+if (day == 6 || day == 7)
+{
+    Console.WriteLine("Этот день является выходным");
+}
+if (day == 1 || day == 2 || day == 3 || day == 4 || day == 5)
+{
+    Console.WriteLine("Этот день не является выходным");
+}
+if (day < 1 || day > 7)
+{
+    Console.WriteLine("В неделе семь дней");
+}
