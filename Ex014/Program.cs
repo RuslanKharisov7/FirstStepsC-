bea961a02ec2cc2f7c2257711bdc6b93e9fdbcdf
KharisovR <alkatrazrudi@gmail.com>
2023-02-01 23:34:00 +0200

    Решили задачу 13 из домашнего задания, второго семинара

---
+++ Ex014/Program.cs
@@ -0,0 +1,17 @@
+﻿// 14.Найти третью цифру числа или сообщить, что её нет**
+
+Console.Clear();
+Console.WriteLine("Введите число");
+string s = Console.ReadLine();
+int number = int.Parse(s);
+
+if (s.Length < 3)
+{
+    Console.WriteLine("В данном числе нет третьей цифры");
+    return;
+}
+else
+{   
+     string lastnumber = Convert.ToString(s[2]);
+    Console.WriteLine($"Третья цифра заданого числа {lastnumber}");
+}
