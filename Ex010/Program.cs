0dd1ccb045216bdca40cac62f359691dbed3b0dc
KharisovR <alkatrazrudi@gmail.com>
2023-02-01 22:54:44 +0200

    Решили задачу №10, домашнее задание второй семинар

---
+++ Ex010/Program.cs
@@ -0,0 +1,7 @@
+﻿// 10.Показать вторую цифру трёхзначного числа**
+
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int number = int.Parse(Console.ReadLine()!);
+int rez = (number/10)%10;
+Console.WriteLine($"Вторая цифра числа {number}, цифра {rez}");
