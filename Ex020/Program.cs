273e0b361ec21f2f2753c4708aceacb239e2397c
KharisovR <alkatrazrudi@gmail.com>
2023-02-03 00:32:13 +0200

    Решили задачу 20 из доп задач

---
+++ Ex020/Program.cs
@@ -0,0 +1,21 @@
+﻿// 20. Задать номер четверти, показать диапазоны для возможных координат
+
+Console.Clear();
+Console.WriteLine("Введите номер четверти");
+int number = int.Parse(Console.ReadLine()!);
+if (number == 1)
+{
+    Console.WriteLine("Диапазон заданной четверти {x>0 && y>0}");
+}
+if (number == 2)
+{
+    Console.WriteLine("Диапазон заданной четверти {x<0 && y>0}");
+}
+if (number == 3)
+{
+    Console.WriteLine("Диапазон заданной четверти {x<0 && y<0}");
+}
+if (number == 4)
+{
+    Console.WriteLine("Диапазон заданной четверти {x>0 && y<0}");
+}
