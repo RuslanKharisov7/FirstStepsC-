b153718230177d385ffcfd2ca27b35aa599eb651
KharisovR <alkatrazrudi@gmail.com>
2023-02-03 23:41:38 +0200

    Решили 21 задачу, дополнительные задания

---
+++ Ex021/Program.cs
@@ -0,0 +1,21 @@
+﻿// 21. Программа проверяет пятизначное число на палиндромом.
+
+Console.Clear();
+Console.WriteLine("Введите пятизначное число от 10000 до 99999");
+int number = int.Parse(Console.ReadLine()!);
+int a = number/10000;
+int b = (number/1000)%10;
+int c = (number/100)%10;
+int d = (number/10)%10;
+int e = number%10;
+
+if (a+b == d+e)
+{
+  Console.WriteLine($"Введенное число {number}, является палиндромом");
+}
+ else 
+ {
+    Console.WriteLine($"Число {number}, не является палиндромом");
+ }
+
+
