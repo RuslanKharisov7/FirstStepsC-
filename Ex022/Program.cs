9468c4d8a581f317078e3a0b977545a2f76ad512
KharisovR <alkatrazrudi@gmail.com>
2023-02-04 00:04:22 +0200

    Решили задачу 22, из доп заданий

---
+++ Ex022/Program.cs
@@ -0,0 +1,14 @@
+﻿// 22. Найти расстояние между точками в пространстве 2D/3D
+
+Console.Clear();
+Console.WriteLine("Введите координаты точки A");
+int xa = int.Parse(Console.ReadLine()!);
+int ya = int.Parse(Console.ReadLine()!);
+int za= int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите координаты точки B");
+int xb = int.Parse(Console.ReadLine()!);
+int yb = int.Parse(Console.ReadLine()!);
+int zb = int.Parse(Console.ReadLine()!);
+double dist = (Math.Sqrt(Math.Pow(xb-xa,2)+Math.Pow(yb-ya,2)+Math.Pow(zb-za,2)));
+Console.WriteLine($"Расстояние между заданными точками {dist}");
+
