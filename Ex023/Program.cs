2129b4aa80f8ac93d82664b8e6fdc6d472c2edf3
KharisovR <alkatrazrudi@gmail.com>
2023-02-09 23:30:39 +0200

    Решили задачу N23, из домашнего задания

---
+++ Ex023/Program.cs
@@ -0,0 +1,11 @@
+﻿// 23. Показать таблицу квадратов чисел от 1 до N 
+
+
+Console.WriteLine("Введите целое натуральное число");
+double number = double.Parse(Console.ReadLine()!);
+double M = 1;
+while(M<=number)
+{
+    Console.Write($"{(Math.Pow(M,3))}, ");
+    M++;
+}
