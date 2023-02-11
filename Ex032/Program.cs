4aa8df4fcca2ce2d6a8cae29f71257ffee5345db
KharisovR <alkatrazrudi@gmail.com>
2023-02-12 01:36:58 +0200

    Решили 32 задачу на семинаре

---
+++ Ex032/Program.cs
@@ -0,0 +1,18 @@
+﻿// 32. Задать массив из 8 элементов, заполненных нулями и единицами вывести их на экран 
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int number = int.Parse(Console.ReadLine()!);
+int [] array = GetBinArray(number);
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int[] GetBinArray (int size)
+{
+    int[] result = new int [size];
+    for(int i=0; i<size; i++)
+    {
+        result[i]= new Random().Next(2);
+    }
+    return result;
+}
+
