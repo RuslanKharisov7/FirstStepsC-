dd080e387be0c5dbe363f99c29c4eb317a865014
KharisovR <alkatrazrudi@gmail.com>
2023-02-11 23:53:37 +0200

    Решили задачу 024 на семинаре

---
+++ Ex024/Program.cs
@@ -0,0 +1,12 @@
+﻿//25. Найти сумму чисел от 1 до А
+Console.Clear();
+Console.WriteLine("Введите число A");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Сумма чисел от 1 до {A} равна {GetSum(A)}");
+int GetSum (int A)
+{
+    int sum = 0;
+    for (int i = 1; i<= A; i++)
+    sum+=i;
+    return sum;
+}
