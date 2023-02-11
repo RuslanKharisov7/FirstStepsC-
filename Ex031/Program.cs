4b4866c887c09e5bc58b93918d2d67388d7b3384
KharisovR <alkatrazrudi@gmail.com>
2023-02-12 01:52:38 +0200

    Решили домашнее задание N31

---
+++ Ex031/Program.cs
@@ -0,0 +1,18 @@
+﻿// 31. Задать массив из 8 элементов и вывести их на экран 
+
+Console.Clear();
+Console.WriteLine("Введите размерность массива");
+int number = int.Parse(Console.ReadLine()!);
+int [] array = GetBinArray(number);
+
+Console.WriteLine($"[{String.Join(",", array)}]");
+
+int[] GetBinArray (int size)
+{
+    int[] result = new int [size];
+    for(int i=0; i<size; i++)
+    {
+        result[i] = new Random().Next();
+    }
+    return result;
+}
