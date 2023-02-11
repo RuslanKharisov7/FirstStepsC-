128f6abed1d99a4bac2b9dd09a376bdbda51cc32
KharisovR <alkatrazrudi@gmail.com>
2023-02-12 00:23:13 +0200

    Решили 27 задачу на семинаре

---
+++ Ex027/Program.cs
@@ -0,0 +1,16 @@
+﻿// 27. Определить количество цифр в числе
+
+Console.Clear();
+Console.WriteLine("Введите число");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Количество цифр в числе = {CountNumbers(A)}");
+int CountNumbers (int A)
+{
+  int count = 0;
+  while(A>0)
+  {
+  count++;
+  A /=10;
+  }
+  return count;
+}
