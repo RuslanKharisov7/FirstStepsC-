7480aaf60b5ca3ce6c0c95238db936132164e710
KharisovR <alkatrazrudi@gmail.com>
2023-02-12 02:03:50 +0200

    Решили задачу 29 на семинаре

---
+++ Ex029/Program.cs
@@ -0,0 +1,14 @@
+﻿// 29. Написать программу вычисления произведения чисел от 1 до N**
+
+Console.Clear();
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+Console.WriteLine($"Произведение чисел от 1 до {N} = {ResNumber(N)}");
+
+int ResNumber (int N)
+{
+   int result = 1;
+        for(int i = 2; i<=N; i++)
+        result*=i;
+        return result;
+}
