1d2e22b71439f38f36e50464699be3f197516ba9
KharisovR <alkatrazrudi@gmail.com>
2023-02-12 02:01:21 +0200

    Решили 26 задачу из домашнего задания

---
+++ Ex026/Program.cs
@@ -0,0 +1,15 @@
+﻿// 26. Возведите число А в натуральную степень B используя цикл
+
+Console.Clear();
+Console.WriteLine("Введите число A");
+double A = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите чтсло B");
+double B = double.Parse(Console.ReadLine()!);
+Console.WriteLine($"Число {A} в степени {B} = {getPow(A,B)}");
+
+double getPow (double A,double B)
+{
+  double result = (Math.Pow(A, B));
+  return result;
+}
+
