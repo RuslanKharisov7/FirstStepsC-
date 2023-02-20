4fcef37a502dc7fce07c22b15da161765bc18534
KharisovR <alkatrazrudi@gmail.com>
2023-02-21 00:08:51 +0200

    Решили задачу 35 из домашнего задания

---
+++ Ex037/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 35:Задайте одномерный массив из 123 случайных чисел [0: 1000].
+//Найдите количество элементов массива, значения которых лежат в отрезке [10,99].
+
+int[] Array = GetArray (123, 0, 1000);
+Console.WriteLine(String.Join(" ", Array));
+int[] GetArray(int size, int minValue, int maxValue)
+{
+    int[] res = new int[size];
+    for (int i = 0; i<size; i++)
+    {
+      res[i] = new Random().Next(minValue, maxValue +1);
+    }
+    return res;
+}
+
+int count = 0;
+for( int i=0; i<Array.Length; i++)
+{
+    if(Array[i] >= 10 & Array[i]<=99)
+    count++;
+}
+Console.WriteLine($"В заданом массиве отрезку (10,99) принадлежат {count} чисел");
