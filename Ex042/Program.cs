f7b9b97fc778269af769812d4f2142041defe2ed
KharisovR <alkatrazrudi@gmail.com>
2023-02-23 23:38:28 +0200

    Решили задачу 42 из домашнего задания

---
+++ Ex042/Program.cs
@@ -0,0 +1,24 @@
+﻿// 42. Определить сколько чисел больше 0 введено с клавиатуры
+
+Console.WriteLine("Введите размерность массива");
+int number = int.Parse(Console.ReadLine()!);
+int[] array= new int [number];
+
+for (int i =0; i<array.Length; i++)
+{
+  Console.Write($"Введите элемент массива под индексом {i}:\t");
+  array[i] = int.Parse(Console.ReadLine()!);
+}
+Console.WriteLine("Вывод массива");
+for (int i =0; i<array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+}
+Console.WriteLine();
+int count = 0;
+for(int i=0; i<array.Length; i++)
+{
+  if (array[i]> 0)
+  count++;
+}
+Console.WriteLine($"Введенно {count} чисел больше 0 ");
