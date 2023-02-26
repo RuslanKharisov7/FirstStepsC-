5fdf7ea8e3ffe4c4afb87e5ef617bc3bf1941611
KharisovR <alkatrazrudi@gmail.com>
2023-02-26 20:28:24 +0200

    Решение задач на 4 лекции

---
+++ Lection4/Factorial/Program.cs
@@ -0,0 +1,13 @@
+﻿// Factorial
+
+
+double Factorial(double n)
+{
+if(n==1) return 1;
+else return n*Factorial(n-1);
+}
+for(double i = 1; i<40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
---
+++ Lection4/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задание двумерного массива
+
+void FillArray(int[,]matr)
+{
+ for(int i=0; i<matr.GetLength(0); i++)
+ {
+    for(int j=0; j<matr.GetLength(1); j++)
+    {
+      matr[i,j] = new Random().Next(1, 10);
+    }
+ }
+}
+
+void PrintArray(int[,] matr)
+{
+  for (int rows=0; rows<matr.GetLength(0); rows++)
+ {
+    for(int columns=0; columns<matr.GetLength(1); columns++)
+    {
+        Console.Write($"{matr[rows,columns]} ");
+    }
+    Console.WriteLine();
+  }
+}
+
+int[,] matrix = new int [3,5];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
