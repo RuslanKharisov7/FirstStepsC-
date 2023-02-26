02fa48235818839207a76145178bcd2f4783b94d
KharisovR <alkatrazrudi@gmail.com>
2023-02-26 20:52:33 +0200

    Решили задачу 48 задание двумерного массива

---
+++ Ex048/Program.cs
@@ -0,0 +1,29 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+void FillArray(int[,]matrix)
+{
+    for(int i=0; i<matrix.GetLength(0); i++)
+    {
+        for(int j=0; j<matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray(int[,]matrix)
+{
+ for(int i=0; i<matrix.GetLength(0); i++)
+ {
+    for(int j=0; j<matrix.GetLength(1); j++)
+    {
+        Console.Write($"{matrix[i,j]}");
+    }
+    Console.WriteLine();
+ }
+}
+int[,] matrix = new int [3,4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
