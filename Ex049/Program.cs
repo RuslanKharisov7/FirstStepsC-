bdcf01f4d1c8e0f3a8c02026db711366a44a5279
KharisovR <alkatrazrudi@gmail.com>
2023-02-26 23:22:58 +0200

    Решили задачу 48 на семинаре

---
+++ Ex049/Program.cs
@@ -0,0 +1,35 @@
+﻿// 
+
+// Задайте двумерный массив размера m на n, каждыый элемент в массиве находится по формуле : A[i][j]=i+j
+// вывести полученный массив на экран
+// m =3; n =4;
+
+
+void FillArray(int[,] matrix)
+{
+    for(int i=0; i<matrix.GetLength(0); i++)
+    {
+        for(int j=0; j<matrix.GetLength(1); j++)
+        {
+            matrix[i,j]= i+j;
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for(int i=0; i<matrix.GetLength(0); i++)
+    {
+        for(int j=0; j<matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] matrix = new int [3,4];
+PrintArray(matrix);
+FillArray(matrix);
+Console.WriteLine();
+PrintArray(matrix);
+
