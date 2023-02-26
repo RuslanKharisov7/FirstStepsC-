a86793a39c6fb2ee19244664e468452f769bfca5
KharisovR <alkatrazrudi@gmail.com>
2023-02-26 22:22:14 +0200

    Задали двумерный массив с участием пользтелем

---
+++ Ex048Var2/Program.cs
@@ -0,0 +1,33 @@
+﻿// 48. Показать двумерный массив размером m×n заполненный целыми числами
+
+Console.WriteLine("Введите количество строк массива");
+int rows = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите количество столбцов массива");
+int columns = int.Parse(Console.ReadLine()!);
+
+int[,] getArray(int m, int n, int minValue, int maxValue)
+{
+    int[,] result = new int[m,n];
+    for(int i=0; i<m; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            result[i,j]= new Random().Next(minValue, maxValue+1);
+        }
+    }
+    return result;
+}
+
+void PrintArray(int[,] inArray)
+{   
+    for(int i=0; i<inArray.GetLength(0); i++)
+    {
+        for(int j=0; j<inArray.GetLength(1); j++)
+        {
+          Console.Write($"{inArray[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] array = getArray(rows, columns, 0, 10);
+PrintArray(array);
