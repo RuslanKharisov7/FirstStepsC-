ba8ec7705ecd01c7ecf62ae60ba55ba8e8fddb91
KharisovR <alkatrazrudi@gmail.com>
2023-03-16 00:00:07 +0200

    Решили задачу 64 из дз

---
+++ Ex064/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+Console.Clear();
+Console.Write("Введите число N: ");
+int N = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(PrintNumbers(N));
+string PrintNumbers(int N)
+{
+    if(N==1)
+    {
+        Console.WriteLine(N);
+        return N.ToString();
+    }
+    string s= N.ToString() + " " + PrintNumbers(N-1);
+    Console.WriteLine(s);
+    return s;
+}
+
+
